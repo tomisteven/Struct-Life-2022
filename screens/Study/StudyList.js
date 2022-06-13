f186353a6aa9875915b7972b96a135aed0bbf5e3
Tomas Steven <tomssteven22@gmail.com>
2022-06-13 12:46:39 -0300

    v2

--- screens/Study/StudyList.js
+++ screens/Study/StudyList.js
@@ -1,15 +1,16 @@
 import React, {
     useState,
-    useEffect
+    useEffect,
+    
 } from 'react'
-import { Text, View, StyleSheet ,TouchableHighlight, Image, ActivityIndicator, ScrollView, ProgressBarAndroidComponent } from 'react-native'
-//import * as Progress from 'react-native-progress';
-
+import { Text, View, StyleSheet ,TouchableHighlight, Image, ActivityIndicator, ScrollView, Modal} from 'react-native'
+import AwesomeAlert from 'react-native-awesome-alerts';
+import { Alert } from 'react-native';
 import ProgressBar from 'react-native-progress/Bar';
 import {
   useFonts
 } from '@expo-google-fonts/inter';
-
+import DialogInput from 'react-native-dialog-input-custom';
 import confg from "../../congiruracionGlobal"
 
 
@@ -24,9 +25,11 @@ export default function StudyList() {
   const {url} = confg
 
     const [study, setStudy] = useState({
-        loading: true,
-        data: []
+        data: [],
     })
+    const [isLoading, setLoading] = useState(true)
+    const [isModalVisibleEdit, setModalVisibleEdit] = useState(false)
+    const [isModalVisibleDelete, setModalVisibleDelete] = useState(false)
 
     const getStudys = async () => {
       try {
@@ -40,15 +43,57 @@ export default function StudyList() {
         const _data = await study.json()
   
         setStudy({
-          loading: false,
-          data: _data.studies
+          data: _data.studies,
         })
+        setLoading(false)
+        setModalVisibleEdit(false)
+
       } catch (error) {
         console.log(error);
       }
     }
 
-    console.log(study);
+    const deleteStudy = async (id) => {
+      const study =  await fetch(`${url}/api/studies/deletestudy/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      })
+      const _data = await study.json()
+      getStudys()
+    }
+
+    const userUrlId = async (id) => {
+      
+    }
+
+
+    const updateStudy = async (id, number) => {
+      try {
+        const study =  await fetch(`${url}/api/studies/editstudy/${id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify({
+            porcentajeAvance: number
+          })
+        })
+        const saveed = await study.json()
+        
+        getStudys()
+        console.log(id, number);
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+
+    //console.log(study);
 
     useEffect(() => {
         getStudys()
@@ -62,7 +107,6 @@ export default function StudyList() {
       textPresentation:{
         marginTop: 50,
         marginBottom: 20,
-        
         fontSize: 40,
         textAlign: 'center',
         fontFamily:"Rampart",
@@ -91,10 +135,11 @@ export default function StudyList() {
         shadowOpacity: 0.50,
           shadowRadius: 4.50,
           elevation: 3,
+          
     },
     cont_study_item:{
       width: '95%',
-      height: 130,
+      height: 150,
       flexDirection: 'row',
       justifyContent: 'flex-start',
       alignItems: 'center',
@@ -174,24 +219,61 @@ export default function StudyList() {
         marginLeft: 20,
         bottom:  3,
         height: 20,
+      },
+      opacity:{
+        opacity: 0.5
+      },
+      noOpacity:{
+        opacity: 1
+      },
+      actions:{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '100%',
+        marginTop: 10,
+      },
+      text_action:{
+        fontSize: 15,
+        
+        
+        paddingBottom: 5,
+        paddingTop: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        borderRadius: 40,
+        shadowColor: "#000",
+      shadowOffset: {
+        width: 0.7,
+        height: 2,
+        
+    }
+      },
+
+      img_delete:{
+        width: 20,
+        height: 20
       }
+      
       
     })
 
   return (
     <>
       <View>
-        <Text style={study.loading ? styles.noVisible : styles.textPresentation}>
+        <Text style={ isLoading ? styles.noVisible : styles.textPresentation}>
           Lista de <Text style={{color: "#f4a261"}}>Estudio</Text>
         </Text>
       </View>
 
       <ScrollView >
 
+        
 
-          <View style={study.loading ? styles.loader : styles.cont_userlist}>
+
+          <View style={ isLoading ? styles.loader : styles.cont_userlist}>
             {
-              study.loading ? 
+              isLoading ? 
                   <View  style={styles.loader}>
                     <ActivityIndicator size="large" color="#FE7092" />
                   </View>
@@ -199,7 +281,12 @@ export default function StudyList() {
                   study.data.map((item) => (
                     <View key={item._id} style={styles.cont_study_item}>
                       <View style={styles.img_study_item}>
-                        <Image source={require("../../assets/studyList.png")} style={styles.img_study}/>
+                        {
+                          item.porcentajeAvance != 100 ?
+                          <Image source={require("../../assets/studyList.png")} style={styles.img_study}/>
+                          :
+                          <Image source={require("../../assets/Finalizado!.png")} style={styles.img_study}/>
+                        }
                       </View>
                       <View style={styles.item_suty_info}>
                         <View style={styles.view_item_study_materia}>
@@ -210,10 +297,51 @@ export default function StudyList() {
                             <ProgressBar progress={item.porcentajeAvance / 100} height={8} borderColor={"transparent"} unfilledColor={"#84BDFF"} width={180} />
                             <Text style={styles.item_study_porcentaje}>{item.porcentajeAvance}%</Text>
                         </View>
+                        <View style={styles.actions}>
+                          <TouchableHighlight style={styles.cont_text_actions} onPress={() => {
+                            setModalVisibleEdit(true);
+                          } }>
+                            <Image source={require("../../assets/editar.png")} style={styles.img_delete}/>
+                          </TouchableHighlight>
+                          <TouchableHighlight style={styles.cont_text_actions} onPress={() => {
+                            setModalVisibleDelete(true);
+
+                          } }>
+                            <Image source={require("../../assets/eliminar.png")} style={styles.img_delete}/>
+                          </TouchableHighlight>
+                        </View>
+                            
+
                       </View>
+                    
+                      <Modal visible={isModalVisibleDelete}>
+                      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                        <Text style={{fontSize: 20, fontFamily: 'Rampart', fontWeight: "400"}}>
+                          ¿Estas seguro de eliminar este estudio?
+                        </Text>
+                        <View style={{flexDirection: 'row', justifyContent: 'space-around', width: '100%'}}>
+                          <TouchableHighlight onPress={() => {
+                            setModalVisibleDelete(!isModalVisibleDelete)
+                          }}>
+                            <Text style={{fontSize: 20, fontFamily: 'Rampart', fontWeight: "400", color: '#0066FF'}}>
+                              Cancelar
+                            </Text>
+                          </TouchableHighlight>
+                          <TouchableHighlight onPress={() => console.log(item._id)}>
+                            <Text style={{fontSize: 20, fontFamily: 'Rampart', fontWeight: "400", color: '#0066FF'}}>
+                              Eliminar
+                            </Text>
+                          </TouchableHighlight> 
+                        </View>
+                      </View>
+                    </Modal>
+
                     </View>
+
+
                   ))
             }
+
           </View>
 
       </ScrollView>
